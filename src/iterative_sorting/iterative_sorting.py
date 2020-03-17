a5b4343ca1a907782923a9a04621c7ee7c91416b
Ryan State <rwstate@gmail.com>
2020-03-17 14:52:45 -0400

    finish count_sort stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -32,9 +32,28 @@ def bubble_sort(arr):
 
     return arrCopy
 
-
-print(bubble_sort([1, 5, 2, 7, 6, 3]))
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
+    if len(arr) < 2:
+      return arr
+    if maximum < 0:
+      maximum = max(arr)
+    # Take a count array to store the count of each unique object.
+    count = [0] * (maximum + 1)
+    for i in arr:
+      if i < 0:
+        return 'Error, negative numbers not allowed in Count Sort'
+      count[i] += 1
+    # Modify the count array such that each element at each index stores the sum of previous counts.
+    for i in range(0, len(count)):
+      if i:
+        count[i] += count[i - 1]
 
-    return arr
+    # Output each object from the input sequence followed by decreasing its count by 1.
+    output = [0] * len(arr)
+    for i in arr:
+      output[count[i] - 1] = i
+      count[i] -= 1
+    return output
+
+print(count_sort([1, 5, 4, 4, 7, 9, 2, 3, 3]))
